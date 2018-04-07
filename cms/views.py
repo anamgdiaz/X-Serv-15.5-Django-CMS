934e604b46e468c5122373ec2fcffa24ebb36bbc
anamgdiaz <anamariagdiaz@hotmail.es>
2018-04-07 14:35:46 +0200

    Version 1

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pages
 
 # Create your views here.
+
+def look_pages(request,identificador):
+	try:
+		pag = Pages.objects.get(name = identificador)
+		ans = pag.page
+	except Pages.DoesNotExist:
+		ans = "No hay datos en la base de datos"
+	return HttpResponse(ans)
+
+def list_pages(request):
+	ans = "Lista de páginas: <br/>"
+	pages_list = Pages.objects.all()
+	for pg in pages_list:
+		ans += pg.name + ' es ' + str(pg.page)+"<br/>"
+	return HttpResponse(ans)
